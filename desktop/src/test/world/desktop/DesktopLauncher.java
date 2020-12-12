f340d8bb2eda7eca6c1c42f6ad3435bba9f37c82
Rondeo Balos <mr.ron669@gmail.com>
2020-12-12 22:33:52 +0800

    initial commit

---
+++ desktop/src/test/world/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package test.world.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import test.world.TestWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestWorld(), config);
+	}
+}
